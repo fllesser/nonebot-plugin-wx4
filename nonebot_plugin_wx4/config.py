4ddab04a4efbbc5eca0599bf8f67dcf62669f1e7
Les Freire <64878354+fllesser@users.noreply.github.com>
2024-12-11 01:55:28 +0800

    update nonebot_plugin_wx4/config.py

--- nonebot_plugin_wx4/config.py
+++ nonebot_plugin_wx4/config.py
@@ -1,22 +1,12 @@
 from pydantic import BaseModel  
-  
+from nonebot import get_plugin_config
+
 class MyPluginConfig(BaseModel):  
     # 在这里定义你的配置项  
-    DBNAME: str  
-    SUPER_ID: list[str]  
-    GROUP_LIST: list[str]  
-    API_KEY: str
-    SECRET_KEY: str
-    MAX_MESSAGES: int
-
-  
-    class Config:
-
-        #数据库名字
-        DBNAME="wxbot.db"
-
-        API_KEY = "" 
-        SECRET_KEY = ""   
-
-        #最大对话记忆
-        MAX_MESSAGES = 5  
+    DBNAME: str = "wxbot.db"
+    API_KEY: str = ""
+    SECRET_KEY: str = ""
+    MAX_MESSAGES: int = 5 
+    
+    
+wx_config = get_plugin_config(MyPluginConfig)
