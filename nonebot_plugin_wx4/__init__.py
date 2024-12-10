ac63571ab4babd4e01401b13dda5624a678e4e1c
Les Freire <64878354+fllesser@users.noreply.github.com>
2024-12-11 01:29:31 +0800

    update nonebot_plugin_wx4/__init__.py

--- nonebot_plugin_wx4/__init__.py
+++ nonebot_plugin_wx4/__init__.py
@@ -1,6 +1,6 @@
 from nonebot import on_command
 from nonebot.adapters import Event
-from nonebot.adapters.onebot.v11 import Message,PrivateMessageEvent
+from nonebot.adapters.onebot.v11 import Bot, Message, PrivateMessageEvent, GroupMessageEvent, MessageEvent, MessageSegment
 from nonebot.params import CommandArg
 from .ConversationStorage import ConversationStorage
 from nonebot.plugin import PluginMetadata
@@ -19,53 +19,40 @@ __plugin_meta__ = PluginMetadata(
 
     supported_adapters={"~onebot.v11",},
 )
-Config=MyPluginConfig.Config
 
-wx=on_command("文心",block=True, priority=1)
-clear_wx = on_command("失忆术", block=True, priority=1)
+Config = MyPluginConfig.Config
+
+wx = on_command("%", block = True, priority = 1)
+clear_wx = on_command("***", block = True, priority = 1)
   
-wxbot=ConversationStorage(Config.DBNAME)
+wxbot = ConversationStorage(Config.DBNAME)
 
 @wx.handle()
-async def wx_handle(foo:Event,cmd: Message = CommandArg()):
-    #指定的群聊或者私聊
-    user_id,group_id=get_id(foo)
-    if grouplim(user_id,group_id) or isinstance(foo, PrivateMessageEvent):
-        pass
-    else:
-        print(111)
-        return
-    
-    content = cmd.extract_plain_text()
-    await wx.send('请稍等')
-    res=await wxbot.send_message(user_id,group_id,content)
-    await wx.finish(res)
+async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
+    user_id, group_id = get_id(event)
+    content = args.extract_plain_text().strip()
+    if reply := event.reply:
+        content += " " + reply.message.extract_plain_text().strip()
+    await bot.call_api("set_msg_emoji_like", message_id = event.message_id, emoji_id = '282')
+    msg_list = await wxbot.send_message(user_id, group_id, content)
+    nickname = (await bot.get_login_info())['nickname']
+    username = (await bot.get_group_member_info(group_id=group_id, user_id=user_id))['nickname']
+    nodes = []
+    for msg in msg_list:
+        if msg['role'] is 'assistant':
+            uid = bot.self_id
+            name = nickname
+        else:
+            uid = user_id
+            name = username
+        nodes.append(MessageSegment.node_custom(user_id=uid, nickname=name, content=msg['content'])
+    await wx.finish(nodes)
 
 @clear_wx.handle()
-async def clear_wx_func(foo:Event):
-    user_id,group_id=get_id(foo)
-    wxbot.clear(user_id,group_id)
-    await clear_wx.finish("失忆了")
+async def _(event: MessageEvent):
+    user_id, group_id = get_id(event)
+    wxbot.clear(user_id, group_id)
+    await clear_wx.finish("clear finished")
 
-def get_id(foo):
-    try:
-        dt=foo.get_session_id().split('_')
-        group_id=str(dt[1])
-        user_id=str(dt[2])
-    except:
-        dt=foo.get_user_id()
-        user_id=str(dt[1])
-        group_id=""
-    return user_id,group_id
-
-def grouplim(user_id,group_id):
-    if user_id in Config.SUPER_ID:
-        return True
-
-    if group_id=="":
-        return False
-
-    if group_id in Config.GROUP_LIST:
-        return True
-    else:
-        return False
+def get_id(event: MessageEvent) -> int, int:
+    return event.user_id, event.group_id if isinstance(event, GroupMessageEvent) else None
