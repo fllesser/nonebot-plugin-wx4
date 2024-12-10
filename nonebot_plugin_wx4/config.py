3643069fa2198cd0e93a7c3d0ed0beb750b2ade1
Les Freire <64878354+fllesser@users.noreply.github.com>
2024-12-11 00:59:54 +0800

    update nonebot_plugin_wx4/config.py

--- nonebot_plugin_wx4/config.py
+++ nonebot_plugin_wx4/config.py
@@ -10,13 +10,7 @@ class MyPluginConfig(BaseModel):
     MAX_MESSAGES: int
 
   
-    class Config:  
-        #指定群聊，群号
-        #指定群所有人都可以调用文心
-        GROUP_LIST=[]
-        #超级ID，QQ号
-        #超级ID可以在任意群调用文心
-        SUPER_ID=[]
+    class Config:
 
         #数据库名字
         DBNAME="wxbot.db"
