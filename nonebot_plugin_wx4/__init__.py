4d52f30ae216c513fe6502fb52c5f0b0bb1c6271
Les Freire <64878354+fllesser@users.noreply.github.com>
2024-12-11 17:19:49 +0800

    update nonebot_plugin_wx4/__init__.py

--- nonebot_plugin_wx4/__init__.py
+++ nonebot_plugin_wx4/__init__.py
@@ -26,6 +26,7 @@ wx = on_command("%", block = True, priority = 1)
 clear_wx = on_command("***", block = True, priority = 1)
   
 wxbot = ConversationStorage(wx_config.DBNAME)
+await wxbot.init_access_token()
 
 @wx.handle()
 async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
@@ -36,21 +37,23 @@ async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
     if content.strip() == "":
         return
     await bot.call_api("set_msg_emoji_like", message_id = event.message_id, emoji_id = '282')
-    msg_list = await wxbot.send_message(user_id, group_id, content)
-    nickname = (await bot.get_login_info())['nickname']
-    username = (await bot.get_group_member_info(group_id=group_id, user_id=user_id))['nickname']
-    nodes = []
-    msg_list.reverse()
-    for msg in msg_list:
-        if msg['role'] == 'assistant':
-            uid, name = bot.self_id, nickname
-        else:
-            uid, name = user_id, username
-        if content := msg.get('content'):
-            if "```" in content:
-                content = MessageSegment.image(await md_to_pic(md=content))
-            nodes.append(MessageSegment.node_custom(user_id=uid, nickname=name, content=content))
-    await wx.finish(nodes)
+    ai_reply = await wxbot.send_single_message(content)
+    await wx.send(MessageSegment.image(await md_to_pic(md=ai_reply)), at_sender=True)
+    # msg_list = await wxbot.send_multi_message(user_id, group_id, content)
+    # nickname = (await bot.get_login_info())['nickname']
+    # username = (await bot.get_group_member_info(group_id=group_id, user_id=user_id))['nickname']
+    # nodes = []
+    # msg_list.reverse()
+    # for msg in msg_list:
+    #     if msg['role'] == 'assistant':
+    #         uid, name = bot.self_id, nickname
+    #     else:
+    #         uid, name = user_id, username
+    #     if content := msg.get('content'):
+    #         if "```" in content:
+    #             content = MessageSegment.image(await md_to_pic(md=content))
+    #         nodes.append(MessageSegment.node_custom(user_id=uid, nickname=name, content=content))
+    # await wx.finish(nodes)
 
 @clear_wx.handle()
 async def _(event: MessageEvent):
