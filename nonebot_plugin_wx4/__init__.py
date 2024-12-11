f9c1b5f8c8ec032b7c6978d02e059bbf7fb1a331
Les Freire <64878354+fllesser@users.noreply.github.com>
2024-12-12 00:16:04 +0800

    update nonebot_plugin_wx4/__init__.py

--- nonebot_plugin_wx4/__init__.py
+++ nonebot_plugin_wx4/__init__.py
@@ -2,7 +2,7 @@ from nonebot import on_command, logger, require, get_driver
 from nonebot.adapters import Event
 from nonebot.adapters.onebot.v11 import Bot, Message, PrivateMessageEvent, GroupMessageEvent, MessageEvent, MessageSegment
 from nonebot.params import CommandArg
-from .ConversationStorage import ConversationStorage
+from .ConversationStorage import WxClient
 from nonebot.plugin import PluginMetadata
 from .config import Config, wx_config
 require("nonebot_plugin_htmlrender")
@@ -24,14 +24,15 @@ __plugin_meta__ = PluginMetadata(
 )
 
 wx = on_command("%", block = True, priority = 1)
-clear_wx = on_command("***", block = True, priority = 1)
+# clear_wx = on_command("***", block = True, priority = 1)
   
-wxbot = ConversationStorage(wx_config.DBNAME)
+# wx_client = ConversationStorage(wx_config.DBNAME)
+wx_client = WxClient(wx_config.wx_api_key, wx_config.wx_secret_key)
 
 @get_driver().on_startup
 async def _():
-    await wxbot.init_access_token()
-    logger.info(f"wx4 init, access_token: {wxbot.access_token}")
+    await wx_client.init_access_token()
+    logger.info(f"wx4 init, access_token: {wx_client.access_token}")
 
 
 @wx.handle()
@@ -52,7 +53,7 @@ async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
         await bot.call_api("set_msg_emoji_like", message_id = event.message_id, emoji_id = '38')
         return
     await bot.call_api("set_msg_emoji_like", message_id = event.message_id, emoji_id = '282')
-    ai_reply = await wxbot.send_single_message(content)
+    ai_reply = await wx_client.send_message(content)
     if "```" in ai_reply:
         msg = [
             MessageSegment.reply(event.message_id),
@@ -64,7 +65,7 @@ async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
             MessageSegment.node_custom(user_id = bot.self_id, nickname = 'wx4', content = ai_reply)
         ]
     await wx.send(msg)
-    # msg_list = await wxbot.send_multi_message(user_id, group_id, content)
+    # msg_list = await wx_client.send_multi_message(user_id, group_id, content)
     # nickname = (await bot.get_login_info())['nickname']
     # username = (await bot.get_group_member_info(group_id=group_id, user_id=user_id))['nickname']
     # nodes = []
@@ -80,11 +81,11 @@ async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
     #         nodes.append(MessageSegment.node_custom(user_id=uid, nickname=name, content=content))
     # await wx.finish(nodes)
 
-@clear_wx.handle()
-async def _(event: MessageEvent):
-    user_id, group_id = get_id(event)
-    wxbot.clear(user_id, group_id)
-    await clear_wx.finish("clear finished")
+# @clear_wx.handle()
+# async def _(event: MessageEvent):
+#     user_id, group_id = get_id(event)
+#     wx_client.clear(user_id, group_id)
+#     await clear_wx.finish("clear finished")
 
 def get_id(event: MessageEvent) -> (int, int):
     return event.user_id, event.group_id if isinstance(event, GroupMessageEvent) else None
